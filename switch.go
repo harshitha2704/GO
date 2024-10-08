cd1ff3e4fd4d1cc5da070290e28a67bf30d8e30c
harshitha2704 <harshitharao2704@gmail.com>
2024-10-08 12:52:05 +0530

    weekdays

---
+++ switch.go
@@ -0,0 +1,27 @@
+package main
+import("fmt")
+func getweekday(day int )string{
+	switch day{
+	case 1:
+		return "monday"
+	case 2:
+		return "tuesday"
+	case 3:
+		return "wednesday"
+	case 4:
+		return "thursady"
+	case 5:
+		return "friday"
+	case 6:
+		return "saturday"
+	case 7:
+		return "sunday"
+	default:
+		return "invalid day "
+	}
+}
+func main(){
+	day := 3
+	weekdayname := getweekdayname(day)
+	fmt.Println(weekdayname)
+}
